cb41c3c3b9e6898a3d6cfc1893fd148ef5dd32ad
RavenApiata <RavenApiata@gmail.com>
2016-05-04 14:31:50 +1200

    Added import functions.

--- Code/Camera-Base-Positioning.cpp
+++ Code/Camera-Base-Positioning.cpp
@@ -3,14 +3,19 @@
 
 #include <stdio.h>
 
+extern "C" int take_picture();
+extern "C" char get_pixel(int row, int col, int color);
+
 int main () {
 
    /**INITIAL SETUP*/
-   //Array used to store the processed camera output (1 for line detected, 0 for white)
+   //Array used to store the processed camera output (1 for line detected, 0 for white).
    int processedCameraOutput[ 320 ];
-
    //Array used to determine the position of offset of the line detected.
    int offsetPosition[ 320 ];
+
+   //Stores the current dispaly on the camera.
+   take_picture();
 
    //Sets values from -160 to 159 in an array (includes 0).
    int value = -160;
