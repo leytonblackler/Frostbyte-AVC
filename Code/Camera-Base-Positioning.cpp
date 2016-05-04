c2b5dfcd628dc1568afa5c9b3c83ab335294415f
Jess <jessica.q.chiu@gmail.com>
2016-05-04 14:33:04 +1200

    Created method for finding pixels in a row.

--- Code/Camera-Base-Positioning.cpp
+++ Code/Camera-Base-Positioning.cpp
@@ -17,6 +17,26 @@ int main () {
    //Stores the current dispaly on the camera.
    take_picture();
 
+   //Processes the image and stores line detection in the processedCameraOutput array.
+   for (int column = 0; column < 320; column++) {
+        //Reads the RGB values for the given pixel.
+        int red = get_pixel(120, column, 0);
+        int green = get_pixel(120, column, 1);
+        int blue = get_pixel(120, column, 2);
+
+        //Finds average colour from RGB values.
+        int averageColour = (red+green+blue)/3;
+
+        //Stores eithe a 1 or 0 in the array at the position of the pixel.
+        if (averageColour >= 180) {
+            //Line detected.
+            processedCameraOutput[column] = 1;
+        } else {
+            //No line detected.
+            processedCameraOutput[column] = 0;
+        }
+   }
+
    //Sets values from -160 to 159 in an array (includes 0).
    int value = -160;
    for (int i = 0; i < 320; i++) {
