7a9d3e445a994fc9d9ae40761840aeca12f9ad4a
Leyton Blackler <leytonblackler@gmail.com>
2016-05-06 14:53:34 +1200

    Created a loop for main section of code.

--- Project-Code/Camera-Base-Positioning.cpp
+++ Project-Code/Camera-Base-Positioning.cpp
@@ -33,62 +33,69 @@ extern "C" int receive_from_server(char message[24]);
 
 int main () {
 
-   /**INITIAL SETUP*/
-   
-   init(0);
-   // connect camera to the screen
-   open_screen_stream();
-   
-   //Array used to store the processed camera output (1 for line detected, 0 for white).
-   int processedCameraOutput[ 320 ];
-   //Array used to determine the position of offset of the line detected.
-   int offsetPosition[ 320 ];
-   //Array used to find error code.
-   int errorCodeArray[ 320 ];
-   //Variable usedto store the error code.
-   int errorCode = 0;
+    /**INITIAL SETUP*/
 
-   //Stores the current display on the camera.
-   take_picture();
+    init(0);
+    // connect camera to the screen
+    open_screen_stream();
 
-   //Processes the image and stores line detection in the processedCameraOutput array.
-   for (int column = 0; column < 320; column++) {
-        //Reads the RGB values for the given pixel.
-        int red = get_pixel(120, column, 0);
-        int green = get_pixel(120, column, 1);
-        int blue = get_pixel(120, column, 2);
+    //Array used to store the processed camera output (1 for line detected, 0 for white).
+    int processedCameraOutput[ 320 ];
+    //Array used to determine the position of offset of the line detected.
+    int offsetPosition[ 320 ];
+    //Array used to find error code.
+    int errorCodeArray[ 320 ];
+    //Variable used to store the error code.
+    int errorCode = 0;
 
-        //Finds average colour from RGB values.
-        int averageColour = (red+green+blue)/3;
-        printf("Average colour: %d\n", errorCode);
+    bool run = true;
 
-        //Stores eithe a 1 or 0 in the array at the position of the pixel.
-        if (averageColour >= 180) {
-            //Line detected.
-            processedCameraOutput[column] = 1;
-        } else {
-            //No line detected.
-            processedCameraOutput[column] = 0;
-        }
-   }
-
-   //Sets values from -160 to 159 in an array (includes 0).
-   int value = -160;
-   for (int i = 0; i < 320; i++) {
+    //Sets values from -160 to 159 in an array (includes 0).
+    int value = -160;
+    for (int i = 0; i < 320; i++) {
         offsetPosition[i] = value;
         value++;
-   }
-
-   //Finds error code based on arrays.
-   for (int i = 0; i < 320; i++) {
-        errorCodeArray[i] = processedCameraOutput[i] * offsetPosition[i];
-   }
-   //Adds all the values in the errorCode array.
-   for (int i = 0; i < 320; i++) {
-        errorCode = errorCode + errorCodeArray[i];
-        printf("Final error code: %d\n", errorCode);
     }
-    
-    //Waits for 5 seconds.
-    Sleep(5,0);
+
+    //** MAIN LOOP */
+    while (run) {
+        //Stores the current display on the camera.
+        take_picture();
+
+        //Processes the image and stores line detection in the processedCameraOutput array.
+        for (int column = 0; column < 320; column++) {
+            //Reads the RGB values for the given pixel.
+            int red = get_pixel(120, column, 0);
+            int green = get_pixel(120, column, 1);
+            int blue = get_pixel(120, column, 2);
+
+            //Finds average colour from RGB values.
+            int averageColour = (red+green+blue)/3;
+            printf("Average colour: %d\n", errorCode);
+
+            //Stores either a 1 or 0 in the array at the position of the pixel.
+            if (averageColour >= 180) {
+                //Line detected.
+                processedCameraOutput[column] = 1;
+            } else {
+                //No line detected.
+                processedCameraOutput[column] = 0;
+            }
+        }
+
+        //Finds error code based on arrays.
+        for (int i = 0; i < 320; i++) {
+            errorCodeArray[i] = processedCameraOutput[i] * offsetPosition[i];
+        }
+
+        //Adds all the values in the errorCode array.
+        for (int i = 0; i < 320; i++) {
+            errorCode = errorCode + errorCodeArray[i];
+            printf("Final error code: %d\n", errorCode);
+        }
+
+        //Waits for 5 seconds.
+        Sleep(5,0);
+    }
+    return 0;
 }
